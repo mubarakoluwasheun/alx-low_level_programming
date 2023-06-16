b9d9b51b445d1006ecad3a3b84c34c186701e655
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 11:11:28 -0500

    A C program that prints using 'put's function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,8 @@
  * desription - The program prints a word using the function 'put'
  *
  * return - Return 0 to indicate successful program execution
+ *
+ * return value of main - main returns the string inside the puts function
  */
 
 int main(void)
