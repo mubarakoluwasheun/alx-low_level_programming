95e0e4e0d33c37e05e9d758d8f7830dae952f24f
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 12:34:28 -0500

    A C program that prints using 'fwrite' function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Desription - The program prints the size of various types using 'print>
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
