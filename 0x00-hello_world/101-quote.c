8990e0dd8c869548e03adc46a75444078837637f
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 12:44:47 -0500

    A C program that prints using 'write' function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	putc("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 }
