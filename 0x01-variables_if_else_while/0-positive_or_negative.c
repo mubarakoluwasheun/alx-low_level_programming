7954b47d43f734d566f1520671462ae2b03384d1
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 17:58:20 -0500

    A C program that generates a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,19 +17,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("%d\n", n);
+	printf("%d", n);
 
 	if (n > 0)
 	{
-		printf("is positive.\n");
+		printf(" is positive");
 	}
 	else if (n == 0)
 	{
-		printf("is zero.\n");
+		printf(" is zero");
 	}
 	else
 	{
-		printf("is negative.\n");
+		printf(" is negative");
 	}
 	return (0);
 }
