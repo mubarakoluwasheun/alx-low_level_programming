972be5b05a8ae1c72d526390e21723974df6cc8f
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-15 23:57:43 -0500

    A C program that prints using 'put's function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 /**
-* main - Entry point of the program
-*
-* The program prints a word using the function 'put'
-*
-* Description : Return 0 to indicate successful program execution
-*/
+ * main - Entry point of the program
+ *
+ * The program prints a word using the function 'put'
+ *
+ * Description : Return 0 to indicate successful program execution
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
