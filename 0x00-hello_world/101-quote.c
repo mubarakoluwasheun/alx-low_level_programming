3e861bcf264156067f5d98e50ee6a4dab2070094
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 13:19:18 -0500

    A C program that prints using 'fwrite' function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	char *message;
-	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	fwrite(message, sizeof(char), strlen(message), stdout);
 	return (0);
 }
