008cd6f39eb93cffe4f1622d5ac4a5850837a704
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 17:49:14 -0500

    A C program that generates a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -20,15 +21,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf(is positive\n);
+		printf("is positive.\n");
 	}
 	else if (n == 0)
 	{
-		printf(is zero\n);
+		printf("is zero.\n");
 	}
 	else
 	{
-		printf(is negative\n);
+		printf("is negative.\n");
 	}
 	return (0);
 }
