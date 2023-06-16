21a8e7d8f0aebdbff6cd07dd9f361610df4a4a55
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-15 23:59:50 -0500

    A C program that prints using 'printf' function

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,14 +11,12 @@
 int main(void)
 {
 	int i;
-	double d;
 	char c;
 	float f;
 	long int li;
 	long long int lli;
 
 	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
 	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
 	printf("The size of a float is: %f.\n", (unsigned long)sizeof(f));
 	printf("The size of a long int is: %ld.\n",(unsigned long)sizeof(li));
