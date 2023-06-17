9bd7b0c47e5bfe455c3ae7a61cb35fc76aeff44b
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 21:14:53 -0500

    A C program that prints alphabet without some alphabet in lowercase using the putchar() function

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Desription - The program prints the alphabet in lowercase using putcha>
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+		char lowercase = tolower(alphabet);
+
+		putchar(lowercase);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
