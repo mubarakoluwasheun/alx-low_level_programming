9ca29429ecf0b995c433f3a09fa686758c4a78f0
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 23:19:30 -0500

    A C program that prints all possible combinations of a single digit numbers using putchar()

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Desription - The program prints the alphabet in lowercase using putcha>
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(',');
+		}
+	}
+	putchar('\n')
+	return (0);
+}
