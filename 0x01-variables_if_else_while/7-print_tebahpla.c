ce6c6a83d1659e5f74966374f4307af9e07d8ae6
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 22:05:21 -0500

    A C program that prints lowercase alphabet in reverse using putchar()

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Desription - The program prints the alphabet in lowercase using putcha>
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet >= 'z'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
