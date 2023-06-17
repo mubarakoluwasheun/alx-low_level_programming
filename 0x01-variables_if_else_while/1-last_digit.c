7e15cdda4277999c13652dd84da7695aa9760696
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 19:20:29 -0500

    A C program that generates a random number and prints the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,17 +22,17 @@ int main(void)
 
         if (lastDigit > 5)
         {
-                printf(" and is greater than 5\n", n);
+                printf(" and is greater than 5\n");
         }
         else
 {
 		if (lastDigit == 0)
         {
-                printf(" and is 0\n", n);
+                printf(" and is 0\n");
         }
         	else
         {
-                printf(" and is less than 6 and not 0\n", n);
+                printf(" and is less than 6 and not 0\n");
 	}
 }
 	return (0);
