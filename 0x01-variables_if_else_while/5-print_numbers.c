0d31edb31fe973171e3e03bbf6f6ed0b5ebef112
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-16 21:39:39 -0500

    A C program that prints all signle digits numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Desription - The program prints the alphabet in lowercase using putcha>
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int base10;
+
+	for (base10 == 0; base10 <= 10; base10++)
+	{
+		printf("%d\n", base10);
+	}
+	return (0);
+}
