483dc256dabfcd6ba219fb3a68a6e95b36250d55
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-17 17:18:27 -0500

    A C program that prints all possible combinations of two two digit numbers using putchar()

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,13 +16,14 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			if (num1 != 98 || num2 != 99)
+			putchar('0' + num1 / 10);
+			putchar('0' + num1 % 10);
+			putchar(' ');
+			putchar('0' + num2 / 10);
+			putchar('0' + num2 % 10);
+
+			if (num1 != 99 || num2 != 99)
 			{
-				putchar('0' + num1 / 10);
-				putchar('0' + num1 % 10);
-				putchar(' ');
-				putchar('0' + num2 / 10);
-				putchar('0' + num2 % 10);
 				putchar(',');
 				putchar(' ');
 			}
