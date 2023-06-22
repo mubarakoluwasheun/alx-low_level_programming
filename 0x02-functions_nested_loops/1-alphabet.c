7fd77e16346d211fc248cbc4b71b7d9c774999d5
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-22 02:19:43 +0100

    A program that prints _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Desription - The program prints the size of various types using 'p>
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+
