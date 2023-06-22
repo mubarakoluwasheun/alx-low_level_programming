ead23a3c7c7c40753370143d3f0181218b2ef381
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-22 08:30:21 -0500

    A function that returns 0 or 1 if the alphabet is in lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A function that prints 10× alphabet in lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
