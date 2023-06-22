ca9e2f68837fdc3ba3fb692b86267898980dd3d6
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-22 18:04:52 -0500

    A program that prints the uppercase alphabet

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Desription - The program prints the alphabet in lowercase using putcha>
+ *
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		putchar(c);
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
