f0947db3dd98d6a3896aa3be6090da5d2e02d843
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-22 03:13:27 +0100

    A funtion that prints the alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,6 +4,7 @@
  * main - Entry point of the program
  *
  * Desription - The program prints the lower case of alphabets
+ * Function print alphabet - prints lowercase alphabets
  *
  * Return: Always 0 (Success)
  */
