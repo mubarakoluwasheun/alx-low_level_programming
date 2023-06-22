ba6722b1a843f858c04b268978d7a6f29124e252
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-22 02:42:04 +0100

    A funtion that prints the alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,23 @@
 /**
  * main - Entry point of the program
  *
- * Desription - The program prints the size of various types using 'p>
+ * Desription - The program prints the lower case of alphabets
  *
  * Return: Always 0 (Success)
  */
+void print_alphabet(void);?
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		putchar(lower[i]);
+	}
+	putchar('\n');
+	return (0);
+
 int main()
 {
-
+	print_alphabet();
+	return (0);
+}
