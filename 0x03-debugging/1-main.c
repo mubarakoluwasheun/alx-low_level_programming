231789fce5d9c0bc4ff2ccf78dcb1bd58838276c
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-22 16:37:30 -0500

    An infinite loop comment out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 /**	int i;
-
+ */
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
+/**	i = 0;
 
 	while (i < 10)
 	{
 		putchar(i); This statment causes the infinite loop
- */	}
-
+	}
+ */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
