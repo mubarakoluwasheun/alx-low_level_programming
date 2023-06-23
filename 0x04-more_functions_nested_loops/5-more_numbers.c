2ddbcf91b4197836dbfa52da4fb77475c84dba79
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-22 21:52:17 -0500

    A program that print number 0 - 9 x 10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,9 +16,11 @@ void more_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		int j;
+
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar('0' + i);
+			_putchar('0' + j);
 		}
 	}
 	_putchar('\n');
