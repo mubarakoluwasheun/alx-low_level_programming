6fee0da838e9e143b7faf09e4ecc5d9fe692b5ec
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-22 21:28:43 -0500

    A program that print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_numbers - print integers 0 to 9
+ * print_most_numbers - print integers 0 to 9 except 2 and 4
  *
  * Description - print integers using _putchar
  *
@@ -10,12 +10,16 @@
  * Return: return 0 (Success)
  */
 
-void print_numbers(void);
+void print_most_numbers(void);
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
 		_putchar('0' + i);
 	}
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,8 @@
 
 void print_numbers(void);
 
+void print_most_numbers(void);
+
 int mul(int a, int b);
 
 int _isdigit(int c);
