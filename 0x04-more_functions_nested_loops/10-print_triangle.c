b7e0bb3084ee532195be20ff483b3c25994cfa9c
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-23 18:28:28 -0500

    A programm that prints # in triangle form

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,6 +15,7 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
 	if (size <= 0)
 	{
@@ -22,7 +23,11 @@ void print_triangle(int size)
 	}
 	for (i = 0; i <= size; i++)
 	{
-		for (j = 0; j <= i; j++)
+		for (j = 0; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
 		{
 			_putchar('#');
 		}
