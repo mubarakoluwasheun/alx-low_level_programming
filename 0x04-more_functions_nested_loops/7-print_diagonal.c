8ad6a6751ce2f336dabd5fab0baf262e14088b7c
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-23 12:45:31 -0500

    A program that print symbol (\)

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,15 +17,13 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 	}
-	else
+	for (int i = 0; i < n; i++)
 	{
-		int i;
-
-		for (i = 0; i < n; i++)
+		for (int j = 0; j < i; j++)
 		{
-			_putchar(' ')
-			_putchar('\\');
+			_putchar(' ');
 		}
+		_putchar('\\');
 		_putchar('\n');
 	}
 }
