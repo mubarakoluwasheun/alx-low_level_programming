75392d2b8caa02f70e69fdcffcfbfa8de01b5ae0
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-22 21:04:19 -0500

    A program that multiplies two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,48 +2,20 @@
 #include <stdio.h>
 
 /**
- * Mul - multiply two integers
+ * mul - multiply two integers
  *
  * Description - print the multiples of integers
  *
- * _putchar - display the output
  * @a: first input digit
  * @b: second input digit
- * multiple: a constant variable
- * digits: array to store individual digits
- * numDigit: number of digits in product
  *
- * Return: returns 0 (Success)
+ * Return: returns multiples
  */
 
 int mul(int a, int b)
 {
-	long long int multiple;
-	multiple = (a * b);
-
-	if (multiple == 0)
+	int mul(int a, int b)
 	{
-		_putchar('0');
-		return (0);
+		return (a * b);
 	}
-	if (multiple < 0)
-	{
-		_putchar('-');
-		multiple = -multiple;
-	}
-	int digits[20];
-	int numDigits = 0;
-
-	while (multiple > 0)
-	{
-		digits[numDigits] = (multiple % 10);
-		multiple = (multiple / 10);
-		numDigits++;
-	}
-
-	for (int i = numDigits - 1; i >= 0; i--)
-	{
-		_putchar('0' + digits[i]);
-	}
-	_putchar('\n');
 }
