fe193851e076618a90d35706aa743660a0a5f385
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-23 13:11:10 -0500

    A program that use the # character to print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - print the square using the character #
+ *
+ * Description - print square using _putchar
+ *
+ * @size: the size of the square
+ *
+ * Return: no return value
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('#');
+		_putchar('\n');
+	}
+}
