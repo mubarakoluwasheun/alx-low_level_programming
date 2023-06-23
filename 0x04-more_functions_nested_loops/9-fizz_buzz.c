89aa9d10a891acea2a15cf9b44adeb52a5ae7bc9
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-23 16:13:50 -0500

    A FizzBuzz test program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - code implementation starts here
+ *
+ * Description - print fizzbuzz according to some conditions
+ *
+ * Return: always 0 (Success)
+ */
+int main (void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if  (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		{
+		else
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
