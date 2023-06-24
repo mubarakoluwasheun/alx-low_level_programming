c0c248d795ea456f9b0f25682bbcb17b95991335
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-24 20:32:20 +0100

    A function that prints the absolute value of an input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - A function that prints the absolute value
+ *
+ * @n: the parameter of the _abs function
+ *
+ * Return:  returns absolute value
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _abs(int);
+
 int _isalpha(int c);
 
 int _islower(int c);
