2d735fa82853f1258d5b53373738cac6c7bb1f00
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-25 04:09:31 +0100

    A function that prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,13 +19,14 @@ void times_table(void)
 		{
 			result = (i * j);
 
-			if (result < 10)
+			if (result <= 9)
 			{
 				_putchar(' ');
 				_putchar(result + '0');
 			}
 			else
 			{
+				_putchar(' ');
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 			}
