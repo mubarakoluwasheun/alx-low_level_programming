d20fe61358aa41e930dc5d1e50a7abe857c84ca0
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-25 13:35:42 +0100

    A function that print the sum of multiples of both 5 and 3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * natural_num - A function that sum the
+ *	         multiples of both 5 and 3
+ *
+ * @n: the parameter of the function
+ *
+ * Return: no return value
+ */
+
+void natural_num(int n)
+{
+	int sum = 0;
+
+	for (n = 0; n <= 1023; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void natural_num(int n);
+
 void print_times_table(int n);
 
 void print_to_98(int n);
