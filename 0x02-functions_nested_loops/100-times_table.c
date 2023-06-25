ed5d75e59b16c6bdf0aef8f4235ab4dc1ad57415
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-25 09:18:37 +0100

    A function that print the times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
-	int result;
+	int product;
 
 	if (n > 15 || n < 0)
 	{
@@ -24,9 +24,25 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			result = i * j;
-			printf("%d\t", result);
+			product = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (product < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (product < 100)
+			{
+				_putchar(' ');
+			}
+			_putchar((product / 100) + '0');
+			_putchar(((product / 10) % 10) + '0');
+			_putchar((product % 10) + '0');
 		}
-		printf("\n");
+		_putchar('\n');
 	}
 }
