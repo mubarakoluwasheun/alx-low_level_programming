8c241f3adb58ed669b7692327641b4f1e2934931
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-25 02:36:39 +0100

    A function that prints every minute of the day of jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,11 +14,11 @@ void jack_bauer(void)
         {
 		for (mins = 0; mins < 60; mins++)
 		{
-			_putchar('0' + hours / 10);
-			_putchar('0' + hours % 10);
+			_putchar('0' + hrs / 10);
+			_putchar('0' + hrs % 10);
 			_putchar(':');
-			_putchar('0' + minutes / 10);
-			_putchar('0' + minutes % 10);
+			_putchar('0' + mins / 10);
+			_putchar('0' + mins % 10);
 			_putchar('\n');
 		}
 	}
