f4a174d67a8f501fcabfb6dd66f43e8668a47717
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-25 07:47:48 +0100

    A function that print the times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - A function that print
+ *		       the times table
+ *
+ * @n: the parameter of the times table function
+ *
+ * Return: no return value
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int result;
+
+	if (n > 15 || n < 0)
+	{
+		break;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+			printf("%d\t", result);
+		}
+		printf("\n");
+	}
+}
