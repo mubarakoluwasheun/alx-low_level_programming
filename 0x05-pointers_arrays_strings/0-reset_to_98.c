71b155e8a44c806a41a0fffdfcb78f63afb664ba
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-27 00:51:51 +0100

    A function that takes a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * Desription - The program takes a pointer
+ *		to an int as parameter
+ *
+ * @n: the parameter of the function
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C-Ppointers, arrays and strings
