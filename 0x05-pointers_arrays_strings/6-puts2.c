c26ff8e52bf2b453ba17731bdc36ce22ce16e102
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-27 03:25:21 +0100

    A function that prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -15,9 +15,14 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
+		if (str[i + 1] == '\0')
+		{
+			break;
+		}
+		i++;
 	}
 	_putchar('\n');
 
