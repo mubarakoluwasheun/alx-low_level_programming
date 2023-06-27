51d9d3bec97a88235fa4ea5e49d43543c75881fb
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-27 01:08:04 +0100

    A function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ * swap_int - Entry point of the program
+ * Desription - The program swaps the values
+ *              of two integers
+ *
+ * @a: the first parameter of the function
+ * @b: the sscond parameter of the function
+ * Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
+} 
