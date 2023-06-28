8a27ed3ff53dd74f8d6308c6ec7bdd9032c49d88
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-28 05:28:00 +0100

    A function that generates random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * generate_password - Entry point of the program
+ * Desription - The program generate a random
+ *              password
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+char *generate_password()
+{
+	char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
+	int i;
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		 password[i] = rand() % 94 + 33;
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+
+	return (password);
+}
