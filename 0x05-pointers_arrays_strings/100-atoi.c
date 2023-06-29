fc9c14c7dedf96899f4d74c53a2b10892584d382
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-29 20:37:10 +0100

    A function that converts string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,38 +16,30 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int i = 0;
+	int digit_count = 0;
 
-	while (s[i] == ' ')
+	while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
 	{
-        	i++;
-	}
-	if (s[i] == '+' || s[i] == '-')
-	{
-		sign = (s[i] == '-') ? -1 : 1;
+		if (s[i] == '+' || s[i] == '-')
+		{
+			sign *= -1;
+		}
 		i++;
 	}
-	while (s[i] != '0' && s[i] <= '9')
+	while (s[i] != '0')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-			{
-				if (sign == -1)
-				{
-					return (INT_MIN);
-				}
-				else
-				{
-					return (INT_MAX);
-				}
-			}
-			result = result * 10 + (s[i] - '0');
-		}
-		else
-		{
+		if (s[i] < '0' || s[i] > '9')
 			break;
-		}
+		result = result * 10 + s[i] - '0');
 		i++;
+		digit_count++;
 	}
+	if (digit_count == 0)
+	{
+		return(0);
+	}
+	else
+	{
 	return (result * sign);
+	}
 }
