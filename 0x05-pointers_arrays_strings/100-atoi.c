cde8027ce9c0bc89a6300a2878b056d338790335
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-29 17:04:11 +0100

    A function that converts string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,23 +17,23 @@ int _atoi(char *s)
 	int result = 0;
 	int i = 0;
 
-	while (s[i] == ' ' || s[i] == '\t')
+	while (s[i] == ' ')
 	{
         	i++;
 	}
 	if (s[i] == '+' || s[i] == '-')
 	{
-		if (s[i] == '-')
-		{
-			sign = -1;
-		}
+		sign = (s[i] == '-') ? -1 : 1;
 		i++;
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+		{
+			return (sign == 1) ? INT_MAX : INT_MIN
+		}
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
-
 	return (result * sign);
 }
