8a8db12e36d66025bc957849999762ebb217b0bc
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-30 04:20:19 +0100

    A function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - Entry point of the program
+ * Desription - The program concatenates
+ *              two strings
+ *
+ * @dest: the first parameter of the function
+ * @src: the sscond parameter of the function
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+	*ptr = *src;
+	ptr++;
+	src++;
+	}
+
+	*ptr = '\0';
+	return dest;
+}
