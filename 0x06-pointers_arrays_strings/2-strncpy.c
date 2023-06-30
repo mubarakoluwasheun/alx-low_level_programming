050af43eb8c7e877ec2b0e9a17cc8f8365388c04
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-06-30 12:34:02 +0100

    A function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,11 +18,11 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dsst[i] = src[i];
+		dest[i] = src[i];
 	}
 	for (; i < n; i++)
 	{
 		dest[i] = '\0';
 	}
-	return dest;
+	return 	(dest);
 }
