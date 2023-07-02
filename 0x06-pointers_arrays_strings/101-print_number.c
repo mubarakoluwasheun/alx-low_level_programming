bbca2cba81e94415e7f64e988f379e06c77d39c5
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-02 10:33:31 +0100

    A function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,29 +9,21 @@
  * Return: Always 0 (Success)
  */
 
-void print_number(int n) {
-    if (n == 0) {
-        _putchar('0');
-        return;
-    }
-
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    int power = 1;
-    int temp = n;
-
-    while (temp > 9) {
-        power *= 10;
-        temp /= 10;
-    }
-
-    while (power > 0) {
-        int digit = n / power;
-        _putchar('0' + digit);
-        n %= power;
-        power /= 10;
-    }
+void print_number(int n)
+{
+	if (n == 0)
+	{
+	       	_putchar('0');
+        	return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar('0' + (n % 10));
 }
