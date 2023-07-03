21485bc493deffafa15a7c8af8caf02c84568ad8
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-03 10:34:55 +0100

    A program that generates the first 50 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,18 +8,23 @@
  */
 int main(void)
 {
-	unsigned int i;
-	unsigned int n = 49;
-	unsigned int fib1 = 1;
-	unsigned int fib2 = 2;
-	unsigned int fib3;
+	int i;
+	int n = 49;
+	unsigned long long fib1 = 1;
+	unsigned long long fib2 = 2;
+	unsigned long long fib3;
 
-	printf("%d, %d, ", fib1, fib2);
+	printf("%llu, %llu, ", fib1, fib2);
 
 	for (i = 2; i <= n; i++)
 	{
 		fib3 = fib1 + fib2;
-		printf("%d, ", fib3);
+		printf("%llu", fib3);
+
+		if (i < n)
+		{
+			printf(", ");
+		}
 		fib1 = fib2;
 		fib2 = fib3;
 	}
