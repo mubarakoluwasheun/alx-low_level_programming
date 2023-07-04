8ec7c0f6c7604e116b051b09fad7595a92947668
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-04 20:09:03 +0100

    A function that locates a character from a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memset - A function that locates a character
+ *           in a string
+ *
+ * @s: first(string) parameter of the function
+ * @c: second(character) parameter of the function
+ *
+ * Return: Always 0(success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return(NULL);
+}
