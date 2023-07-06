72decfaaedefbba52605c720a863c94e7f2c29ca
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-06 13:48:50 +0100

    A function that generates prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,41 @@
 #include "main.h"
 
 /**
- * is_prime_number - A function that prints the prime
+ * is_prime_checker - A function that checks the prime
  *		     number of n
+ *
+ * @n: input (integer) parameter of the function
+ * @i: the iterative paramater of the function
+ *
+ * Return: Always 0(success)
+ */
+int is_prime_recursive(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_recursive(n, i - 1));
+}
+
+/**
+ * is_prime_number - A function that prints the prime
+ *                   number of n
  *
  * @n: input (integer) parameter of the function
  *
  * Return: Always 0(success)
  */
-
-int is_prime_recursive(int n, int i)
-{
-    if (i == 1)
-    {
-        return 1;
-    }
-
-    if (n % i == 0)
-    {
-        return 0;
-    }
-
-    return is_prime_recursive(n, i - 1);
-}
-
 int  is_prime_number(int n)
 {
-    if (n <= 1)
-    {
-        return 0;
-    }
-
-    return is_prime_recursive(n, n - 1);
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (is_prime_recursive(n, n - 1));
 }
