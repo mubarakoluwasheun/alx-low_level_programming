b1bc46ab31f3c1f94db43e6484c622556f54dbe9
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-06 10:16:37 +0100

    A palindrome function

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * is_palindrome - A function that returns 1 if
@@ -9,42 +10,27 @@
  * Return: Always 0(success)
  */
 
-int _strlen_recursion(char *s)
-{
-	int length = 0;
-
-	if (*s == '\0')
-	{
-		int result = length;
-
-		length = 0;
-		return (result);
-	}
-	length++;
-	return (1 + _strlen_recursion(s + 1));
-}
-
 int is_palindrome(char *s)
 {
 	int length;
 	int result;
 
-	length = _strlen_recursion(s);
+	length = strlen(s);
 
 	if (length <= 1)
 	{
-		return 1;
+		return (1);
 	}
 	if (*s != *(s + length - 1))
 	{
-		return 0;
+		return (0);
 	}
 
-	*(s + length - 1) = '\0';
+	*(s + length - 1) = ('\0');
 
 	result = is_palindrome(s + 1);
 
 	*(s + length - 1) = *(s);
 
-	return result;
+	return (result);
 }
