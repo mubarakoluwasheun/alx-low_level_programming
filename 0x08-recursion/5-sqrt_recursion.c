965fa39cf8a31f2b01ba7fa09bb3e21355364370
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-06 15:47:22 +0100

    A function that returns the square root of input

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,44 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - A function that returns the
- *		    square root of a number
+ * _sqrt_recursive - A function that returns the
+ *		     square root of a number
  *
  * @n: input parameter of the function
+ * @currentGuess: the current guess for the sqrt
  *
  * Return: Always 0(success)
  */
 
-int sqrt_recursive(int n, int guess)
+int sqrt_recursive(int n, int currentGuess)
 {
-    if (n < 0)
-    {
-        return -1;
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
 
-    if (guess * guess == n)
-    {
-        return guess;
-    }
+	if (currentGuess * currentGuess == n)
+	{
+		return (currentGuess);
+	}
 
-    if (guess * guess > n)
-    {
-        return sqrt_recursive(n, guess - 1);
-    }
-
-    return sqrt_recursive(n, guess + 1);
+	if (currentGuess * currentGuess > n)
+	{
+		return (sqrt_recursive(n, currentGuess - 1));
+	}
+	return (sqrt_recursive(n, currentGuess + 1));
 }
+
+/**
+ * _sqrt_recursion - A function that generates
+ *                  the natural square root of n
+ *
+ * @n: input (integer) parameter of the function
+ *
+ * Return: Always 0(success)
+ */
 
 int _sqrt_recursion(int n)
 {
-    return sqrt_recursive(n, 0);
+	return (sqrt_recursive(n, 0));
 }
