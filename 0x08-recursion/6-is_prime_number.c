fe1af85d02aec4e3e9cee18a30ad022b59f641ae
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-06 13:25:57 +0100

    A function that generates prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,20 +9,27 @@
  * Return: Always 0(success)
  */
 
-int is_prime_number(int n)
+int is_prime_recursive(int n, int i)
 {
-	int i;
+    if (i == 1)
+    {
+        return 1;
+    }
 
-	if (n <= 1)
-	{
-		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+    if (n % i == 0)
+    {
+        return 0;
+    }
+
+    return is_prime_recursive(n, i - 1);
+}
+
+int  is_prime_number(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+
+    return is_prime_recursive(n, n - 1);
 }
