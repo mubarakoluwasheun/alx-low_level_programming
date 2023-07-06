109e428eed85551c8c2385399e6d7d238edde0c2
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-06 13:07:25 +0100

    A palindrome function

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,17 +11,17 @@
 
 int _strlen_recursion(char *s)
 {
-        int length = 0;
+	int length = 0;
 
-        if (*s == '\0')
-        {
-                int result = length;
+	if (*s == '\0')
+	{
+		int result = length;
 
-                length = 0;
-                return (result);
-        }
-        length++;
-        return (1 + _strlen_recursion(s + 1));
+		length = 0;
+		return (result);
+	}
+	length++;
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -36,22 +36,21 @@ int _strlen_recursion(char *s)
 
 int is_palindrome_checker(char *start, char *end)
 {
-    if (start >= end)
-    {
-        return 1;
-    }
+	if (start >= end)
+	{
+	return 1;
+	}
 
-    if (*start != *end)
-    {
-        return 0;
-    }
+	if (*start != *end)
+	{
+	return 0;
+	}
 
-    return is_palindrome_checker(start + 1, end - 1);
+	return is_palindrome_checker(start + 1, end - 1);
 }
 
 /**
- * is_palindrome - A function that prints a string
- *           follow by a new line
+ * is_palindrome - A function that prints a palindrome
  *
  * @s: first (string) parameter of the function
  *
@@ -60,8 +59,7 @@ int is_palindrome_checker(char *start, char *end)
 
 int is_palindrome(char *s)
 {
-    int length = _strlen_recursion(s);
+	int length = _strlen_recursion(s);
 
-    return (is_palindrome_checker(s, s + length - 1));
+	return (is_palindrome_checker(s, s + length - 1));
 }
-
