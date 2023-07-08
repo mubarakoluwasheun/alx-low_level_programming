79c8cde0bba85c27947a08df21f2146481d06f5b
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-08 04:36:36 +0100

    A function tht prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints all
+ *        arguments it receives
+ *
+ * @argc: Argument count of the function
+ * @argv: Argument vector of the function
+ *
+ * Return: Always 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
