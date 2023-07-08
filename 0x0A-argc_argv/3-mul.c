d2a413948aa26c85db08c606aea57c6073ebafb7
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-08 15:56:52 +0100

    A function that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,28 +2,29 @@
 #include <stdlib.h>
 
 /**
- * main - A function that prints all
- *        arguments it receives
+ * main - A function that multiplies
+ *        two numbers
  *
  * @argc: Argument count of the function
  * @argv: Argument vector of the function
  *
  * Return: Always 0(success)
  */
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
 {
+	int num1;
+	int num2;
+	int mul;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num 2
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
-	int mul = num1 * num2;
-
 	printf("%d\n", mul);
 	return (0);
 }
