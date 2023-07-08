6bd40add54b7f05db82f6d5aaa3b1330e4d5f458
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-08 16:11:50 +0100

    A main.h file

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-
 int main(int argc, char *argv[]);
+
+int main(int argc, char *argv[] __attribute__((unused)));
+
+int main(int argc __attribute__((unused)), char *argv[]);
 
 int _putchar(char c);
 
