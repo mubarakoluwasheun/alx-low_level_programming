cfb66103a8b60756edf54ba006c4a492c1a3ea50
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-09 04:16:23 +0100

    A function that multiplies two numbers[A

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,15 +17,16 @@ int main(int argc, char *argv[])
 	int num2;
 	int mul;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	mul = num1 * num2;
-
-	if (argc != 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+
 	printf("%d\n", mul);
-	return 0;
+	return (0);
 }
