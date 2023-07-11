0e040dcb5c4f24445190ed7fb0beaf887c742f4a
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-11 01:38:00 +0100

    A function that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,7 +10,7 @@
  * Return: Always 0(success)
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int __attribute__((unused)) argca, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
