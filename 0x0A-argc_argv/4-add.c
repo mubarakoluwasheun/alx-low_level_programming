c9e819db8fea7a843e4e669f7ab9622b6404ec03
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-11 00:07:47 -0500

    A function that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,6 +24,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 	}
 	sum = 0;
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -35,6 +36,7 @@ int main(int argc, char *argv[])
 			}
 		}
 		inputNumber = atoi(argv[i]);
+
 		if (inputNumber <= 0)
 		{
 			printf("Error\n");
