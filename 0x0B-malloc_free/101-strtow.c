42108762d041cf313751e296f73e8d16ed2d0afc
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-12 15:48:32 +0100

    A function that splits a strings into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,51 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 
-char **strtow(char *str)
-{
-	int word_count;
-	char *str_copy;
-	char *token;
-	char **words;
-	int index;
-	int i;
+#define MAX_WORD_LENGTH 100
 
+char **strtow(char *str) {
     if (str == NULL || strlen(str) == 0)
         return NULL;
 
-    word_count = 0;
-    str_copy = strdup(str);
-    token = strtok(str_copy, " ");
+    int num_words = 0;
+    int in_word = 0;
+    int i;
 
-    while (token != NULL) {
-        word_count++;
-        token = strtok(NULL, " ");
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ' && !in_word) {
+            num_words++;
+            in_word = 1;
+        } else if (str[i] == ' ') {
+            in_word = 0;
+        }
     }
 
-    words = (char **)malloc((word_count + 1) * sizeof(char *));
-    if (words == NULL) {
-        free(str_copy);
+    char **words = (char **)malloc((num_words + 1) * sizeof(char *));
+    if (words == NULL)
         return NULL;
+
+    int word_index = 0;
+    int word_length = 0;
+    in_word = 0;
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            if (!in_word) {
+                word_length = 0;
+                words[word_index] = (char *)malloc(MAX_WORD_LENGTH * sizeof(char));
+                if (words[word_index] == NULL) {
+                    for (int j = 0; j < word_index; j++)
+                        free(words[j]);
+                    free(words);
+                    return NULL;
+                }
+                in_word = 1;
+            }
+
+            words[word_index][word_length++] = str[i];
+        } else if (in_word) {
+            words[word_index][word_length] = '\0';
+            word_index++;
+            in_word = 0;
+        }
     }
 
-    index = 0;
-    token = strtok(str, " ");
-    while (token != NULL) {
-        words[index] = strdup(token);
-        if (words[index] == NULL) {
-            for (i = 0; i < index; i++)
-                free(words[i]);
-            free(words);
-            free(str_copy);
-            return NULL;
-	}
-        index++;
-        token = strtok(NULL, " ");
+    if (in_word) {
+        words[word_index][word_length] = '\0';
+        word_index++;
     }
 
-    words[index] = NULL;
-
-    free(str_copy);
+    words[word_index] = NULL;
 
     return words;
 }
