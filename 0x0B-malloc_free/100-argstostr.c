65fab30512e53b9e8901e59d7748812163b39602
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-12 14:52:03 +0100

    A function that concatenates arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -46,7 +46,7 @@ char *argstostr(int ac, char **av)
 		conCat[currentIndex] = ('\n');
 		currentIndex++;
 	}
-	_putchar('0' + '\n');
+	conCat[totalLength] = ('\n')
 	conCat[totalLength - 1] = ('\0');
 	return (conCat);
 }
