d4cb19689c1ab179f494c0a2c5a95957c7fa4a20
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-13 15:28:50 -0500

    A function that allocates memory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,7 +9,6 @@
  * @s1: first string parameter
  * @s2: second string parameter
  * @n: number of bytes to allocate
- *
  * Return: returns a pointer to allocated memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -18,38 +17,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0, i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = ("");
-	}
 	else
 	{
 		while (s1[len1])
-		{
 			len1++;
-		}
 	}
 	if (s2 == NULL)
-	{
 		s2 = ("");
-	}
 	else
 	{
 		while (s2[len2] && len2 < n)
-		{
 			len2++;
-		}
 	}
-
 	new_str = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (new_str == NULL)
-	{
 		return (NULL);
-	}
+
 	for (i = 0; i < len1; i++)
-	{
 		new_str[i] = s1[i];
-	}
+
 	for (j = 0; j < len2; j++)
 	{
 		new_str[i] = s2[j];
