dc9f6beed7a804c0f836323ab163bd3b77015d30
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-13 14:27:12 +0100

    A function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,12 @@
 
 /**
  * malloc_checkedD - A function that allocates
- *                   memory using malloc
+ *		     memory using malloc
  *
  * @b: number of bytes to allocate
  *
  * Return: returns a pointer to allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
@@ -21,23 +20,4 @@ void *malloc_checked(unsigned int b)
 		exit(98);
 	}
 	return (ptr);
-}
-
-/**
- * main - A function that prints a string
- *           follow by a new line
- *
- * Return: Always 0(success)
- */
-
-int main()
-{
-	unsigned int size;
-	int *array;
-
-	size = 10;
-	array = malloc_checked(size * sizeof(int));
-
-	free(array);
-	return (0);
 }
