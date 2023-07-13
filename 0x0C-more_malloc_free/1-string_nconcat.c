2724bc7ae9f29b2b211329a8874c3f8dc73d5e6c
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-13 16:59:31 +0100

    A function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 /**
  * string_nconcat - A function that allocates
@@ -13,43 +12,33 @@
  *
  * Return: returns a pointer to allocated memory
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_str;
-	unsigned int len1, len2, i, j;
+    char *new_str;
+    unsigned int len1 = 0, len2 = 0, i, j;
 
-	if (s1 == NULL)
-	{
-		s1 = ("");
-	}
-	if (s2 == NULL)
-	{
-		s2 = ("");
-	}
+    if (s1 == NULL)
+        s1 = "";
+    else
+        while (s1[len1])
+            len1++;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+    if (s2 == NULL)
+        s2 = "";
+    else
+        while (s2[len2] && len2 < n)
+            len2++;
 
-	if (n >= len2)
-	{
-		n = (len2);
-	}
+    new_str = malloc(sizeof(char) * (len1 + len2 + 1));
+    if (new_str == NULL)
+        return (NULL);
 
-	new_str = malloc(sizeof(char) * (len1 + n + 1));
+    for (i = 0; i < len1; i++)
+        new_str[i] = s1[i];
+    for (j = 0; j < len2; j++, i++)
+        new_str[i] = s2[j];
+    new_str[i] = '\0';
 
-	if (new_str == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < len1; i++)
-	{
-		new_str[i] = s1[i];
-	}
-	for (j = 0; j < n; j++, i++)
-	{
-		new_str[i] = s2[j];
-		new_str[i] = ('\0');
-	}
-
-	return (new_str);
+    return (new_str);
 }
