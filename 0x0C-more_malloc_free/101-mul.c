f50cccdb0ee9b92f9ef89d352e059f67d80f18a2
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-14 19:32:26 +0100

    A function that multiplies two integers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,14 +17,14 @@ int multiply_numbers(int num1, int num2)
 }
 
 /**
- * _memset - A function that checks if input
+ * is_positive_integer - A function that checks if input
  *           integer is positive
  *
  * @str: string parameter of the function
  *
  * Return: Always 0(success)
  */
-int is_positive_integer(const char* str)
+int is_positive_integer(const char *str)
 {
 	while (*str)
 	{
@@ -32,37 +32,36 @@ int is_positive_integer(const char* str)
 			return (0);
 		str++;
 	}
-    return (1);
+	return (1);
 }
 
 /**
  * main - Execution of the code starts here
- *        
  *
  * @argc: count parameter of the function
  * @argv: vector parameter of the function
  *
  * Return: Always 0(success)
  */
-int main(int argc, char *argv[]) {
-    int num1, num2;
+int main(int argc, char *argv[])
+{
+	int num1, num2;
 	int result;
 
-    if (argc != 3) {
-        printf("Error\n");
-        return 98;
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if (!is_positive_integer(argv[1]) || !is_positive_integer(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = multiply_numbers(num1, num2);
+	printf("%d\n", result);
 
-    if (!is_positive_integer(argv[1]) || !is_positive_integer(argv[2])) {
-        printf("Error\n");
-        return 98;
-    }
-
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-
-    result = multiply_numbers(num1, num2);
-    printf("%d\n", result);
-
-    return 0;
+	return (0);
 }
