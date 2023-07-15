9f87787ea2eb3659994a3f7e1460f9b1cbcbea66
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-14 22:13:25 -0500

    A program that finds and print the first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,6 +14,9 @@ int main(void)
 	uint64_t fib1 = 1;
 	uint64_t fib2 = 2;
 	uint64_t fib3;
+	uint64_t fib1FirstHalf, fib1SecondHalf;
+	uint64_t fib2FirstHalf, fib2SecondHalf;
+	uint64_t firstHalf, secondHalf;
 
 	printf("%lu, %lu, ", fib1, fib2);
 
@@ -28,6 +31,31 @@ int main(void)
 		}
 		fib1 = fib2;
 		fib2 = fib3;
+	}
+
+	fib1FirstHalf = fib1 / 1000000000000;
+	fib2FirstHalf = fib2 / 1000000000000;
+	fib1SecondHalf = fib1 / 1000000000000;
+	fib2SecondHalf = fib2 / 1000000000000;
+
+	for (i = 2; i <= n; i++)
+	{
+		firstHalf = fib1FirstHalf + fib2FirstHalf;
+		secondHalf = fib1SecondHalf + fib2SecondHalf;
+		if (fib1SecondHalf + fib2SecondHalf > 999999999999)
+		{
+			firstHalf += 1;
+			secondHalf %= 1000000000000;
+		}
+
+		printf("%lu%lu", firstHalf, secondHalf);
+		if (i < n)
+			printf(", ");
+
+		fib1FirstHalf = fib2firstHalf;
+		fib1SecondHalf = fib2SecondHalf;
+		fib2FirstHalf = firstHalf;
+		fib2SecondHalf = secondHalf;
 	}
 	printf("\n");
 	return (0);
