6b7c872dd7629b9772605a91cce36e4e0cb0e81c
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-15 04:54:21 -0500

    A FizzBuz Test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,25 +15,26 @@ int main(void)
 	if (i == 100)
 	{
 		printf("\n");
+
+		for (i = 1; i <= 100; i++)
+		{
+			if  (i % 3 == 0 && i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else if (i % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			else if (i % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%d ", i);
+			}
+		}
+		return (0);
 	}
-	for (i = 1; i <= 100; i++)
-	{
-		if  (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", i);
-		}
-	}
-	return (0);
 }
