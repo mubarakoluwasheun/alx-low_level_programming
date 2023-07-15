2704e8ec604dab2e9c989ab0d8be9d33b4581535
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-15 05:19:30 +0100

    A function that finds and prints the first 98 fibonacci

--- 0x02-functions_nested_loops/104-fib.c
+++ 0x02-functions_nested_loops/104-fib.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/**
+ * main - A function that prints the first
+ *	  50 fibonacci starting 1 and 2
+ *
+ * Return: Always 0(success)
+ */
 int main(void)
 {
 	int i;
@@ -14,26 +20,39 @@ int main(void)
 
 	printf("%lu, %lu, ", fib1, fib2);
 
+	for (i = 2; i <= n; i++)
+	{
+		fib3 = fib1 + fib2;
+		printf("%lu", fib3);
+
+		if (i < n)
+		{
+			printf(", ");
+		}
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+
 	fib1FirstHalf = fib1 / 1000000000000;
 	fib2FirstHalf = fib2 / 1000000000000;
-	fib1SecondHalf = fib1 % 1000000000000;
-	fib2SecondHalf = fib2 % 1000000000000;
+	fib1SecondHalf = fib1 / 1000000000000;
+	fib2SecondHalf = fib2 / 1000000000000;
 
 	for (i = 2; i <= n; i++)
 	{
 		firstHalf = fib1FirstHalf + fib2FirstHalf;
 		secondHalf = fib1SecondHalf + fib2SecondHalf;
-		if (secondHalf > 999999999999)
+		if (fib1SecondHalf + fib2SecondHalf > 999999999999)
 		{
 			firstHalf += 1;
 			secondHalf %= 1000000000000;
 		}
 
-		printf("%lu%012lu", firstHalf, secondHalf);
+		printf("%lu%lu", firstHalf, secondHalf);
 		if (i < n)
 			printf(", ");
 
-		fib1FirstHalf = fib2FirstHalf;
+		fib1FirstHalf = fib2firstHalf;
 		fib1SecondHalf = fib2SecondHalf;
 		fib2FirstHalf = firstHalf;
 		fib2SecondHalf = secondHalf;
