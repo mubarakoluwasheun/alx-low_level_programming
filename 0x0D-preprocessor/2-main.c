93c989db254dba8bad761bc88879344e1a43356d
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-17 17:55:57 -0500

    A header file that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the name of the file
+ *	  it was compiled from
+ *
+ * Return : Always 0(Success)
+ */
+
+int main()
+{
+	printf("File : %s\n", __FILE__);
+	return (0);
+}
