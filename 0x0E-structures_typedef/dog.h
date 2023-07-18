b731308b16d2b3cd80a71804a5fbdee69b948a9b
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-18 05:08:50 +0100

    A type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  * Description: A type of structure name struct dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 /* Function prototype */
 void init_dog(struct dog *d, char *name, float age, char *owner);
