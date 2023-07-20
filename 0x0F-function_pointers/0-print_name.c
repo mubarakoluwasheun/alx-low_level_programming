66fde11312f8bf3fb630f663e6cf8a08cd0e649c
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-19 19:51:45 -0500

    A function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name: A function that print name
+ * print_name - A function that print name
  * @name: the character pointer string
  * @f: a function pointer
  *
@@ -12,6 +12,5 @@ void print_name(char *name, void (*f)(char *))
 	if (name && f)
 	{
 		f(name);
-
 	}
 }
