510dc6d1bc87bb9e365ab27eeb71a29a9b5417ac
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-27 21:50:12 -0500

    A 64-bit program in assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+; Compile with: nasm -f elf64 -o hello.o hello.asm && gcc -o hello hello.o
+; Run with: ./hello
+
+extern printf
+
+section .data
+	message db 'Hello, Holberton', 10; 0
+
+section .text
+	global _start
+
+_start:
+	; Write message to stdout
+	push rbp
+	mov rdi, message
+	xor rax, rax
+	call printf
+	pop rbp
+
+	; Exit program
+	mov rax, 60
+	xor rdi, rdi
+	syscall
