b6cab4fbeeb2ad2507fd911fea54e8ab76603d26
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-27 21:35:40 -0500

    A function that prints a sentences

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * premain - A function that prints a message
+ * before main is executed
+ *
+ * Return: void
+ */
+void __attribute__((constructor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
