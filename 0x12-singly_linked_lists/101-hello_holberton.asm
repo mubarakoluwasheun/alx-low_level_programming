7fdc951227be642d66cc822cb6e6703d9792a5ef
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-07-29 01:07:15 +0100

    A 64-bit program in assembly that prints

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
 section .data
-    format db 'Hello, Holberton', 10, 0
+	format db 'Hello, Holberton', 10, 0
 
 section .text
-    global main
-    extern printf
+	global main
+	extern printf
 
 main:
-    push rbp
-    mov rbp, rsp
-    mov rdi, format
-    xor rax, rax
-    call printf
-    mov rax, 0
-    pop rbp
-    ret
+	push rbp
+	mov rbp, rsp
+	mov rdi, format
+	xor rax, rax
+	call printf
+	mov rax, 0
+	pop rbp
+	ret
