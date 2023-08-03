7c24f9291ac3069afeac7b9550ca1a8fcd8cb7ad
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-08-02 21:36:51 -0500

    A function that prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,37 +1,37 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list
+ * print_listint_safe - A function that prints
+ * a listint_t linked list
  * @head: pointer to the head of the list
  *
- * Return: the number of nodes in the list
+ * Return: Returns the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0, i;
-    const listint_t **array;
+	size_t count = 0, i;
+	const listint_t **array;
 
-    array = malloc(sizeof(listint_t *) * 1024);
-    if (array == NULL)
-        exit(98);
+	array = malloc(sizeof(listint_t *) * 1024);
+	if (array == NULL)
+		exit(98);
 
-    while (head != NULL)
-    {
-        for (i = 0; i < count; i++)
-        {
-            if (array[i] == head)
-            {
-                printf("-> [%p] %d\n", (void *)head, head->n);
-                free(array);
-                return (count);
-            }
-        }
-        printf("[%p] %d\n", (void *)head, head->n);
-        array[count] = head;
-        count++;
-        head = head->next;
-    }
-
-    free(array);
-    return (count);
+	while (head != NULL)
+	{
+		for (i = 0; i < count; i++)
+		{
+			if (array[i] == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(array);
+				return (count);
+			}
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		array[count] = head;
+		count++;
+		head = head->next;
+	}
+	free(array);
+	return (count);
 }
