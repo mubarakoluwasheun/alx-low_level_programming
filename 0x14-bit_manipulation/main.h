ca49f9176e0f4f4287ec624c535c79c0bb2c86d3
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-08-03 16:55:22 -0500

    A function that sets a bit to 1 at a given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 /* Functions prototype */
+int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
