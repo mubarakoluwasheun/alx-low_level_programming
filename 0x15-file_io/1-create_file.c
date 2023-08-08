9ab3891f25455c82debe385e59174d121e7ec454
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-08-07 21:54:49 -0500

    A function that creates a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
-			n_write = write(fd, text_content, len);
+			;
+
+		n_write = write(fd, text_content, len);
 		if (n_write == -1)
 			return (-1);
 	}
