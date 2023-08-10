b1a77791db9e1801ca724ba66dcc23e862c9660d
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-08-09 21:21:39 -0500

    A function that displays the info contains in the ELF header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <elf.h>
 
 /* Functions prototypes */
 int append_text_to_file(const char *filename, char *text_content);
