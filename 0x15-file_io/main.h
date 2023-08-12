3e4b970fa53d47e5aeb05e266afc3b190e91e747
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-08-11 23:08:06 -0500

    A function that displays the info contained in the ELF header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,14 @@
 #include <elf.h>
 
 /* Functions prototypes */
+void print_entry(uint64_t e_entry);
+void print_type(uint16_t e_type);
+void print_abiversion(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
