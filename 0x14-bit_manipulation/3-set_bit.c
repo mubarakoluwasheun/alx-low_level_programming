7ad401da419fcfa333cd5e1dd5de17717aea6e7e
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-09-01 04:25:44 +0100

    A function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - A function that sets the value of a bit
+ * to 1 at a given index
+ * @n: pointer to the number to modify
+ * @index: the index of the bit to set
+ *
+ * Return: Returns 1 if it worked, or -1 if an error
+ * occured
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > (sizeof(*n) * 8))
+		return (-1);
+
+	*n |= (1UL << index);
+	return (1);
+}
