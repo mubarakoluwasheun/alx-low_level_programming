6aa63ff6851d7727cc887985095bcb6b4afe7759
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-11-10 02:31:38 +0100

    a function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,35 +12,37 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *current;
 	unsigned int i;
 
-	if (h == NULL)
-		return (NULL);
-
+	new_node = NULL;
 	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	current = *h;
-	while (current != NULL && current->prev != NULL)
-		current = current->prev;
-
-	for (i = 0; i < idx - 1 && current != NULL; i++)
-		current = current->next;
-
-	if (current == NULL)
-		return (NULL);
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = current->next;
-	new_node->prev = current;
-	if (current->next != NULL)
-		current->next->prev = new_node;
+		new_node = add_dnodeint(h, n);
 	else
-		add_dnodeint_end(h, n);
+	{
+		current = *h;
+		if (current != NULL)
+			for (; current->prev != NULL; current = current->prev);
 
-	current->next = new_node;
+		for (i = 1; current != NULL; current = current->next, i++)
+		{
+			if (i == idx)
+			{
+				if (current->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->next = current->next;
+						new_node->prev = current;
+						current->next->prev = new_node;
+						current->next = new_node;
+					}
+				}
+				break;
+			}
+		}
+	}
 
 	return (new_node);
 }
