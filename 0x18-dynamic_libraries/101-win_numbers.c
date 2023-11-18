8981d15a3c90617e24731748fde6b3f905f20a50
mubarakoluwasheun <mubarakoluwasheun@gmail.com>
2023-11-17 19:46:59 -0600

    Giga millionsss

--- 0x18-dynamic_libraries/101-win_numbers.c
+++ 0x18-dynamic_libraries/101-win_numbers.c
@@ -9,20 +9,21 @@
  */
 int rand(void)
 {
-	static int count = 0;
-	int winning_numbers[] = {9, 8, 10, 24, 75, 9}; // Desired winning numbers
+	static int count;
+	int winning_numbers[] = {9, 8, 10, 24, 75, 9}; /* Desired winning numbers */
 	int total_numbers = sizeof(winning_numbers) / sizeof(winning_numbers[0]);
 
-	// Seed the random number generator
+	count = 0;
+	/* Seed the random number generator */
 	srand(time(NULL));
 
-	// Return the winning numbers sequentially
+	/* Return the winning numbers sequentially */
 	if (count < total_numbers)
 	{
-		return winning_numbers[count++];
+		return (winning_numbers[count++]);
 	}
 	else
 	{
-		return rand(); // Fallback to default rand() behavior
+		return (rand()); /* Fallback to default rand() behavior */
 	}
 }
